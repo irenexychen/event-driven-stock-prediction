9483b62768fdc914ae46a61f13aafbb9bfc748d7
Irene Chen <irene.chen@windriver.com>
2019-03-26 17:56:32 -0400

    leads on news scraping from google rss

---
+++ news-extractor/google_news_rss_extractor.py
@@ -0,0 +1,13 @@
+
+
+import requests
+
+BASE = "https://news.google.com/news/rss"
+
+TOPICS = ["WORLD", "NATION", "BUSINESS", "TECHNOLOGY", "ENTERTAINMENT", "SPORTS", "SCIENCE", "HEALTH"]
+
+TOPIC_URL = "/headlines/section/topic"
+GEO_URL = "/headlines/section/geo"
+
+
+QUERY = "https://news.google.com/rss/search?pz=1&cf=all&q=%22Economy%22&cf=all&scoring=n&hl=en-GB&gl=GB&ceid=GB:en"
