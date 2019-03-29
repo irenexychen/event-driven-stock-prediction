12fa6a93bf6de018c35ab7555f8799246a23fa47
Irene XiangYi Chen <xiangyi99@live.com>
2019-03-28 23:16:02 -0400

    created scraper gnews class

--- news-extractor/google_news_rss_extractor.py
+++ news-extractor/google_news_rss_extractor.py
@@ -1,13 +1,41 @@
 
-
+import json
 import requests
+from bs4 import BeautifulSoup
+
+
 
 BASE = "https://news.google.com/news/rss"
-
 TOPICS = ["WORLD", "NATION", "BUSINESS", "TECHNOLOGY", "ENTERTAINMENT", "SPORTS", "SCIENCE", "HEALTH"]
-
 TOPIC_URL = "/headlines/section/topic"
 GEO_URL = "/headlines/section/geo"
 
-
 QUERY = "https://news.google.com/rss/search?pz=1&cf=all&q=%22Economy%22&cf=all&scoring=n&hl=en-GB&gl=GB&ceid=GB:en"
+
+
+class GNews: 
+	def __init__(self, edition='United States (English)', topic='top stories', location=None, query=None, language='english'):
+        self.edition = edition
+        self.topic = topic
+        self.location = location
+        self.query = query
+        self.language = language
+        self.requestparams = {'output': 'atom',
+                              'ned': self.edition,
+                              'topic': self.topic,
+                              'geo': self.location,
+                              'q': self.query,
+                              'hl': self.language}
+    def send_request: 
+    	entries = soup.findAll('entry')
+    	news_articles = []
+
+        for entry in entries:
+            article = {}
+            article['title'] = entry.title.text
+            article['link'] = entry.link['href']
+            article['releasedAt'] = entry.updated.text
+            articles.append(article)
+
+        return articles
+
